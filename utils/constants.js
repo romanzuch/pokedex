87ab8bdba7f21b7a846736c877a5bfcec2b3d0bd
Roman <roman.zuchowski@gmail.com>
2022-05-01 13:42:10 +0200

    ➕Initial commit constants

---
+++ utils/constants.js
@@ -0,0 +1,83 @@
+export const types = {
+    normal: {
+        id: 1, 
+        color: '#A8A878'
+    },
+    fighting: {
+        id: 2,
+        color: '#C03028'
+    },
+    flying: {
+        id: 3,
+        color: '#A890F0'
+    },
+    poison: {
+        id: 4,
+        color: '#A040A0'
+    },
+    ground: {
+        id: 5,
+        color: '#E0C068'
+    },
+    rock: {
+        id: 6,
+        color: '#B8A038'
+    },
+    bug: {
+        id: 7,
+        color: '#A8B820'
+    },
+    ghost: {
+        id: 8,
+        color: '#705898'
+    },
+    steel: {
+        id: 9,
+        color: '#B8B8CF'
+    },
+    fire: {
+        id: 10,
+        color: '#EF8030'
+    },
+    water: {
+        id: 11,
+        color: '#6890F0'
+    },
+    grass: {
+        id: 12,
+        color: '#78C850'
+    },
+    electric: {
+        id: 13,
+        color: '#F8D030'
+    },
+    psychic: {
+        id: 14,
+        color: '#F85888'
+    },
+    ice: {
+        id: 15,
+        color: '#98D8D8'
+    },
+    dragon: {
+        id: 16,
+        color: '#7038F8'
+    },
+    dark: {
+        id: 17,
+        color: '#705848'
+    },
+    fairy: {
+        id: 18,
+        color: '#F0B6BC'
+    }
+}
+
+export const numberOfPokemon = {
+    base: 150
+}
+
+export const colors = {
+    lightGray: 'E8E8E8',
+    darkGray: '363636'
+}
