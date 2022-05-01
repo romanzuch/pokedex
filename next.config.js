fffaa4571692a4caa12a4a636d7630ab1ad46a7e
Roman <roman.zuchowski@gmail.com>
2022-05-01 13:41:43 +0200

    💡Prepared for local env variables and image sources

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+        'raw.githubusercontent.com'
+    ]
+},
+env: {
+  API_KEY: process.env.API_KEY,
+},
 }
 
 module.exports = nextConfig
