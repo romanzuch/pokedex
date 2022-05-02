b6212d332ed6a271fb8a67a39dfda87dfce28c1e
Roman <roman.zuchowski@gmail.com>
2022-05-02 18:41:23 +0200

    💡Added header to root view

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import PokedexHeader from '../components/PokedexHeader'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <PokedexHeader />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
