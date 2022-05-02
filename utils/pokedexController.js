8c2a5fed52e706130a7855b77c203b18f35ef028
Roman <roman.zuchowski@gmail.com>
2022-05-02 21:14:34 +0200

    💡Added pokedex controller methods

--- utils/pokedexController.js
+++ utils/pokedexController.js
@@ -1,5 +1,39 @@
-function navigatePokedex(props) {
-    console.log(props);
+import axios from "axios";
+
+async function getPokemonList(id, setPokemon, setApiState) {
+    let baseUrl = "https://pokeapi.co/api/v2/pokemon"
+    let requstUrl = `${baseUrl}?limit=150`
+    try {
+        let _ = await axios.get(requstUrl)
+        .then((res) => {
+            let data = res.data;
+            setPokemon(data);
+        });
+    } catch (e) {
+        console.log(typeof(e));
+        return e;
+    }
 }
 
-export { navigatePokedex };
+async function getPokemonDetails(id, setPokemon) {
+    let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    let _ = await axios.get(url)
+    .then((res) => {
+        let data = res.data;
+        setPokemon(data);
+    })
+}
+
+function navigateNext(id) {
+    return true;
+}
+
+function navigateBack(id) {
+    if (id === 1) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+export { navigateNext, navigateBack, getPokemonList, getPokemonDetails };
