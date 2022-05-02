d56e54fd0ff3e2d4b56d33a18a6a908922aed98f
Roman <roman.zuchowski@gmail.com>
2022-05-02 21:11:52 +0200

    💡Refactored code and added states

--- components/PokedexController/index.js
+++ components/PokedexController/index.js
@@ -8,42 +8,87 @@ import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 // util functions
-import { navigatePokedex } from '../../utils/pokedexController';
+import { navigateNext, navigateBack, getPokemonList } from '../../utils/pokedexController';
+import { useRouter } from 'next/router';
 
 // tooltip
 import Tooltip from '@mui/material/Tooltip';
 
+// helpers
+import { useState, useEffect } from 'react';
+
 export default function PokedexController(props) {
+
+    const [pokemonId, setPokemonId] = useState(1); // this is used to store the id of the last pokemon
+    const [pokemon, setPokemon] = useState(0); // this is used to store the information of the pokemon
+    const [apiState, setApiState] = useState(0); // used to fetch errors
+    const [update, setUpdate] = useState(false); 
+
+    const router = useRouter();
+
+    useEffect(() => {
+        let data = getPokemonList(pokemonId, setPokemon);
+        router.push(`pokemon?id=${pokemonId}`)
+    }, [update, pokemonId]);
+
     return (
         <div className={styles.pokedexController}>
             <div className={styles.pokedexControllerInfo}>
                 <h2 className={styles.pokemonNumberWrapper}>
-                    Nr. <span className={styles.pokedexPokemonNumber}>25</span>
+                    Nr. <span className={styles.pokedexPokemonNumber}>
+                        { pokemon ? (<>{pokemonId}</>) : (<></>) }
+                        </span>
                 </h2>
                 <h2 
                     className={styles.pokedexPokemonName}
                 >
-                    {`Pikachu`}
+                    { pokemon ? (<>{pokemon.results[pokemonId-1].name}</>) : (<></>)}
                 </h2>
                 <Tooltip title='Info' placement='top'>
                     <InfoOutlinedIcon 
                         className={styles.pokedexInfoButton}
+                        onClick={() => {console.log(props);}}
                     />
                 </Tooltip>
             </div>
             <div className={styles.pokedexControllerElementsContainer}>
-                <Tooltip title='Previous' placement='top'>
-                    <KeyboardArrowUpIcon 
-                        fontSize='large' 
-                        className={styles.pokedexControllerElement} 
-                        onClick={(buttonProps) => navigatePokedex(buttonProps)}
-                    />
-                </Tooltip>
+                {
+                    (pokemonId === 1) ? (
+                        <Tooltip title='Previous' placement='top'>
+                            <KeyboardArrowUpIcon 
+                                fontSize='large' 
+                                className={styles.pokedexControllerElementDisabled}
+                            />
+                        </Tooltip>
+                     ) : (
+                        <Tooltip title='Previous' placement='top'>
+                            <KeyboardArrowUpIcon 
+                                fontSize='large' 
+                                className={styles.pokedexControllerElement} 
+                                onClick={() => {
+                                    if (navigateBack(pokemonId)) {
+                                        let newId = pokemonId -1;
+                                        setPokemonId(newId);
+                                    } else {
+                                        console.log(false);
+                                    }
+                                }}
+                            />
+                        </Tooltip>
+                    )
+                }
                 <Tooltip title='Next' placement='bottom'>
                     <KeyboardArrowDownIcon 
                         fontSize='large' 
                         className={styles.pokedexControllerElement} 
-                        onClick={(buttonProps) => navigatePokedex(buttonProps)}
+                        onClick={() => {
+                            if (navigateNext(pokemonId)) {
+                                let newId = pokemonId + 1;
+                                setPokemonId(newId);
+                            } else {
+                                console.log(false);
+                            }
+                        }}
                     />
                 </Tooltip>
             </div>
